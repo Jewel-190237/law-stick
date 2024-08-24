863afd13b448d2a6ec3ae9cc2125b0b02626ff00
Jewel-190237 <jewel190237@gmail.com>
2024-08-25 01:25:30 +0600

    file uploaded

--- src/components/HomaPage/HomePage.jsx
+++ src/components/HomaPage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './About';
+import Service from './Service';
 
 const HomePage = () => {
     return (
         <div className='max-w-[1320px] mx-auto'>
 
-            <About></About>
+            <About/>
+            <Service/>
             
         </div>
     );
