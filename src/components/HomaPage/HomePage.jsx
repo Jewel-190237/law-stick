3711d48db6f9db32c67b40d3de4a044c2a9f5156
Jewel-190237 <190237@ku.ac.bd>
2024-08-25 01:24:14 +0600

    file uploaded

--- src/components/HomaPage/HomePage.jsx
+++ src/components/HomaPage/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import About from './About';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='max-w-[1320px] mx-auto'>
+
+            <About></About>
             
         </div>
     );
--- src/components/HomaPage/Service.jsx
+++ src/components/HomaPage/Service.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const About = () => {
+const Service = () => {
     return (
         <div>
-            About
+            
         </div>
     );
 };
 
 
-export default About;
+export default Service;
