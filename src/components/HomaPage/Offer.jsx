c65620052de4984646d6cdf9a72f0b174ca8c1fc
Jewel-190237 <190237@ku.ac.bd>
2024-08-25 01:25:30 +0600

    file uploaded

--- src/components/HomaPage/Offer.jsx
+++ src/components/HomaPage/Offer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Service = () => {
+const Offer = () => {
     return (
         <div>
-            Service
+            
         </div>
     );
 };
 
 
-export default Service;
+export default Offer;
