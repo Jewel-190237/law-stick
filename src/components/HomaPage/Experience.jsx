6e37a1261584049c7f8592b201a67e0f9b0fd931
Jewel-190237 <190237@ku.ac.bd>
2024-08-25 15:32:16 +0600

    file uploaded

--- src/components/HomaPage/Experience.jsx
+++ src/components/HomaPage/Experience.jsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Experience = () => {
     return (
         <div>
-            Expert
+            <div className='mx-auto'>
+                <div className='flex items-center gap-2 '>
+                    <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
+                    <p className='text-[#B68C5A] text-[16px] font-bold'>Latest Top Blog</p>
+                </div>
+                <div className='mt-[25px]'>
+                    <h1 className='heading w-full lg:w-1/2'>Legal Insights & Expert Advice:
+                        Our Blog</h1>
+                </div>
+            </div>
         </div>
     );
 };
