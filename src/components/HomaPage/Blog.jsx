c105e60700e1c573034d6d4180938f2176da609b
Jewel-190237 <jewel190237@gmail.com>
2024-08-25 14:39:56 +0600

    file uploaded

--- src/components/HomaPage/Blog.jsx
+++ src/components/HomaPage/Blog.jsx
@@ -4,9 +4,13 @@ import React from 'react';
 const Blog = () => {
     return (
         <section>
-            <div className='flex items-center gap-3'>
-                <Image src='/public/blogIcon.png' height={50} width={50} alt='Blog Icon' />
-                <p className='text-[#B68C5A] text-[16px] font-bold'>latest top blog</p>
+            <div className='flex items-center gap-2'>
+                <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
+                <p className='text-[#B68C5A] text-[16px] font-bold'>Latest Top Blog</p>
+            </div>
+            <div>
+                <h1 className='heading'>Legal Insights & Expert Advice:
+                Our Blog</h1>
             </div>
         </section>
     );
