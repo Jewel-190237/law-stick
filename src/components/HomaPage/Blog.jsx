ebb424bee1d9d9054f232712fee66ea1e606e95f
Jewel-190237 <jewel190237@gmail.com>
2024-08-25 14:56:09 +0600

    file uploaded

--- src/components/HomaPage/Blog.jsx
+++ src/components/HomaPage/Blog.jsx
@@ -8,13 +8,25 @@ const Blog = () => {
                 <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
                 <p className='text-[#B68C5A] text-[16px] font-bold'>Latest Top Blog</p>
             </div>
-            <div>
-                <h1 className='heading'>Legal Insights & Expert Advice:
-                Our Blog</h1>
+            <div className='mt-[25px]'>
+                <h1 className='heading w-full lg:w-1/2'>Legal Insights & Expert Advice:
+                    Our Blog</h1>
             </div>
-            <div>
-                <p className='description'> Stay informed with our expert legal insights and advice. Our blog covers key topics to help
-                you navigate complex legal matters with confidence.</p>
+            <div className='[40px]'>
+                <p className='description w-full lg:w-1/2'> Stay informed with our expert legal insights and advice. Our blog covers key topics to help
+                    you navigate complex legal matters with confidence.</p>
+            </div>
+            <div className='mt-[40xp] grid grid-cols-1 lg:grid-cols-3 gap-6'>
+                <div className=' bg-[#F9F6EE] rounded-lg p-5'>
+                    <Image src='/blog.png' height={327} width={455} alt='Blog Icon' />
+                </div>
+                <div className='col-span-2 '>
+                    <div className='bg-[#F9F6EE] rounded-lg p-5'>
+
+                    </div>
+
+                </div>
+
             </div>
         </section>
     );
