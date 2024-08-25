0098029ef5dc9b9bb5bb5b5de10104069f661d24
Jewel-190237 <190237@ku.ac.bd>
2024-08-25 16:47:57 +0600

    file uploaded

--- src/components/HomaPage/Experience.jsx
+++ src/components/HomaPage/Experience.jsx
@@ -14,11 +14,22 @@ const Experience = () => {
                 </div>
             </div>
             <div>
-            <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
+                <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2'>
-                <div></div>
-                <div></div>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 content-center'>
+                <div className=''>
+                    <p className='text-[26px] lato mt-10 lg:mt-20'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
+                    <div className='flex gap-2 items-center'>
+                        <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
+                        <div>
+                            <p className='text-xl lato'>Brooklyn Simmons</p>
+                            <p className='text-[14px] lato'>CEO and Founder</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full mx-auto'>
+                    <Image className='mx-auto' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                </div>
 
             </div>
         </section>
