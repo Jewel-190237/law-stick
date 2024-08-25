bf55fc7ca4529e11ba94380f1c7b4c4093237149
Jewel-190237 <jewel190237@gmail.com>
2024-08-25 14:35:27 +0600

    file uploaded

--- src/components/HomaPage/Blog.jsx
+++ src/components/HomaPage/Blog.jsx
@@ -1,10 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            Blog
-        </div>
+        <section>
+            <div className='flex items-center gap-3'>
+                <Image src='/public/blogIcon.png' height={50} width={50} alt='Blog Icon' />
+                <p className='text-[#B68C5A] text-[16px] font-bold'>latest top blog</p>
+            </div>
+        </section>
     );
 };
 
