092e0cd9cb849ac003268b9aaeb2da83e8704ed0
Jewel-190237 <190237@ku.ac.bd>
2024-08-26 10:31:28 +0600

    file uploaded

--- src/components/HomaPage/Experience.jsx
+++ src/components/HomaPage/Experience.jsx
@@ -15,33 +15,44 @@ const Experience = () => {
             </div>
             <div className='bg-[#F9F6EE]'>
                 {/* slider part */}
-                <div className='max-w-[1320px] mx-auto pt-20 pb-8 pl-5'>
-                    <div className='flex flex-col lg:flex-row justify-between'>
-                        <div className='flex-1'>
-                            <div>
-                                <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
-                            </div>
-                            <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
 
-                        </div>
-                        <div className='flex-1'>
-                            <div className='flex justify-between'>
-                                <div> <br /> </div>
-                                <div className=''>
-                                    <Image className='mt-0 lg:-mt-20' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                <div className="carousel w-full">
+                    <div id="slide1" className="carousel-item relative w-full">
+
+                        <div className='max-w-[1320px] mx-auto pt-20 pb-8 pl-5'>
+                            <div className='flex flex-col lg:flex-row justify-between'>
+                                <div className='flex-1'>
+                                    <div>
+                                        <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
+                                    </div>
+                                    <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
+
+                                </div>
+                                <div className='flex-1'>
+                                    <div className='flex justify-between'>
+                                        <div> <br /> </div>
+                                        <div className=''>
+                                            <Image className='mt-0 lg:-mt-20' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className=' mt-10 w-full h-[1px] bg-[#B68C5A]'>
+                                <br />
+                            </div>
+                            <div className='mt-8 flex gap-2 items-center'>
+                                <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
+                                <div>
+                                    <p className='text-xl lato'>Brooklyn Simmons</p>
+                                    <p className='text-[14px] lato'>CEO and Founder</p>
                                 </div>
                             </div>
                         </div>
 
-                    </div>
-                    <div className=' mt-10 w-full h-[1px] bg-[#B68C5A]'>
-                        <br />
-                    </div>
-                    <div className='mt-8 flex gap-2 items-center'>
-                        <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
-                        <div>
-                            <p className='text-xl lato'>Brooklyn Simmons</p>
-                            <p className='text-[14px] lato'>CEO and Founder</p>
+                        <div className="absolute w-1/2 bottom-10 flex gap-6">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
                         </div>
                     </div>
                 </div>
