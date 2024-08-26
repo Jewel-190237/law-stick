9d0385ac3f55eb8e67f9c57e214ca7881f6832c9
Jewel-190237 <190237@ku.ac.bd>
2024-08-26 11:00:27 +0600

    file uploaded

--- src/components/HomaPage/Experience.jsx
+++ src/components/HomaPage/Experience.jsx
@@ -25,6 +25,7 @@ const Experience = () => {
                                     <div>
                                         <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
                                     </div>
+                                    <p className='text-4xl text-red-800'>1</p>
                                     <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
 
                                 </div>
@@ -55,6 +56,147 @@ const Experience = () => {
                                     <div className=" flex gap-4 mt-10">
                                         <a href="#slide4" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❮</a>
                                         <a href="#slide2" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❯</a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+
+                        <div className='max-w-[1320px] mx-auto pt-20 pb-8 pl-5'>
+                            <div className='flex flex-col lg:flex-row justify-between'>
+                                <div className='flex-1'>
+                                    <div>
+                                        <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
+                                    </div>
+                                    <p className='text-4xl text-red-800'>2</p>
+                                    <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
+
+                                </div>
+                                <div className='flex-1'>
+                                    <div className='flex justify-between'>
+                                        <div> <br /> </div>
+                                        <div className=''>
+                                            <Image className='mt-0 lg:-mt-20' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className=' mt-10 w-full h-[1px] bg-[#B68C5A]'>
+                                <br />
+                            </div>
+
+
+                            <div className='flex justify-between items-center'>
+                                <div className='mt-8 flex gap-2 items-center'>
+                                    <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
+                                    <div>
+                                        <p className='text-xl lato'>Brooklyn Simmons</p>
+                                        <p className='text-[14px] lato'>CEO and Founder</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className=" flex gap-4 mt-10">
+                                        <a href="#slide1" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❮</a>
+                                        <a href="#slide3" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❯</a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+
+                        <div className='max-w-[1320px] mx-auto pt-20 pb-8 pl-5'>
+                            <div className='flex flex-col lg:flex-row justify-between'>
+                                <div className='flex-1'>
+                                    <div>
+                                        <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
+                                    </div>
+                                    <p className='text-4xl text-red-800'>3</p>
+                                    <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
+
+                                </div>
+                                <div className='flex-1'>
+                                    <div className='flex justify-between'>
+                                        <div> <br /> </div>
+                                        <div className=''>
+                                            <Image className='mt-0 lg:-mt-20' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className=' mt-10 w-full h-[1px] bg-[#B68C5A]'>
+                                <br />
+                            </div>
+
+
+                            <div className='flex justify-between items-center'>
+                                <div className='mt-8 flex gap-2 items-center'>
+                                    <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
+                                    <div>
+                                        <p className='text-xl lato'>Brooklyn Simmons</p>
+                                        <p className='text-[14px] lato'>CEO and Founder</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className=" flex gap-4 mt-10">
+                                        <a href="#slide2" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❮</a>
+                                        <a href="#slide4" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❯</a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div id="slide4" className="carousel-item relative w-full">
+
+                        <div className='max-w-[1320px] mx-auto pt-20 pb-8 pl-5'>
+                            <div className='flex flex-col lg:flex-row justify-between'>
+                                <div className='flex-1'>
+                                    <div>
+                                        <Image src='/logo.png' height={100} width={100} alt='Blog Icon' />
+                                    </div>
+                                    <p className='text-4xl text-red-800'>4</p>
+                                    <p className='text-[26px] lato mt-10 lg:mt-32'>“ I am incredibly satisfied with the firm’s professionalism and dedication. They kept me informed, supported, and delivered outstanding results. Highly recommend! ”</p>
+
+                                </div>
+                                <div className='flex-1'>
+                                    <div className='flex justify-between'>
+                                        <div> <br /> </div>
+                                        <div className=''>
+                                            <Image className='mt-0 lg:-mt-20' src='/experience.png' height={400} width={400} alt='Blog Icon' />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className=' mt-10 w-full h-[1px] bg-[#B68C5A]'>
+                                <br />
+                            </div>
+
+
+                            <div className='flex justify-between items-center'>
+                                <div className='mt-8 flex gap-2 items-center'>
+                                    <Image src='/quete.png' height={50} width={50} alt='Blog Icon' />
+                                    <div>
+                                        <p className='text-xl lato'>Brooklyn Simmons</p>
+                                        <p className='text-[14px] lato'>CEO and Founder</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className=" flex gap-4 mt-10">
+                                        <a href="#slide3" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❮</a>
+                                        <a href="#slide1" className="py-2 px-3 rounded-md bg-white text-[#B68C5A]">❯</a>
                                     </div>
                                 </div>
 
