48436cf94977cb013639c4f2b3b17e06a15defee
Jewel-190237 <jewel190237@gmail.com>
2024-08-26 11:29:43 +0600

    file uploaded

--- src/components/HomaPage/Team/Team.jsx
+++ src/components/HomaPage/Team/Team.jsx
@@ -3,17 +3,16 @@ import React from 'react';
 
 const Team = () => {
     return (
-        <section className='max-w-[1320px] mx-auto'>
+        <section className='max-w-[1320px] mx-auto mb-20'>
             <div className='flex flex-col items-center justify-center'>
                 <div className='flex items-center gap-2 justify-center'>
                     <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
-                    <p className='text-[#B68C5A] text-[16px] font-bold'>Client Experiences</p>
+                    <p className='text-[#B68C5A] text-[16px] font-bold'>Our Team</p>
                 </div>
                 <div className='mt-[25px] '>
-                    <h1 className='heading text-center'>clients have confident <br className='hidden lg:flex' /> with us </h1>
+                    <h1 className='heading text-center'>the faces of justice</h1>
                 </div>
             </div>
-
         </section>
     );
 };
