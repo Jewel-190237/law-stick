81e9e18c844c1bfc6d2c6c9d28d744399a72749e
Jewel-190237 <190237@ku.ac.bd>
2024-08-26 17:59:16 +0600

    file uploaded

--- src/components/HomaPage/Team/Team.jsx
+++ src/components/HomaPage/Team/Team.jsx
@@ -41,11 +41,12 @@ const Team = () => {
                     centeredSlides={true}
                     slidesPerView={'auto'}
                     coverflowEffect={{
-                        rotate: 50,
+                        rotate: 0,
                         stretch: 0,
                         depth: 100,
-                        modifier: 1,
+                        modifier: 2,
                         slideShadows: true,
+                        loop:true,
                     }}
                     pagination={true}
                     modules={[EffectCoverflow, Thumbs]}
