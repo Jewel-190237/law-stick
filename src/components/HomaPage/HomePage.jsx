f786a58ace0209cd2dbd9f97562c8581cd359b66
Jewel-190237 <190237@ku.ac.bd>
2024-08-26 23:39:37 +0600

    file uploaded

--- src/components/HomaPage/HomePage.jsx
+++ src/components/HomaPage/HomePage.jsx
@@ -6,12 +6,14 @@ import Team from './Team/Team';
 import Expert from './Expert/Expert';
 import Hero from './Hero';
 import AboutUs from './AboutUs';
+import OurService from './OurService';
 
 const HomePage = () => {
     return (
         <div className=''>
             <Hero/>
             <AboutUs/>
+            <OurService/>
             <Team/>
             <Expert/>
             <Experience/>
