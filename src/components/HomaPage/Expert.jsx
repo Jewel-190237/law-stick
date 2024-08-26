d1271417195fc8890cf7fe9fcb78ef11cad402f2
Jewel-190237 <jewel190237@gmail.com>
2024-08-26 11:23:04 +0600

    file uploaded

--- src/components/HomaPage/Expert.jsx
+++ src/components/HomaPage/Expert.jsx
@@ -9,16 +9,25 @@ const Expert = () => {
 
                 </div>
                 <div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <div className='flex items-center gap-2 justify-center'>
+                    <div className='flex flex-col'>
+                        <div className='flex gap-2'>
                             <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
-                            <p className='text-[#B68C5A] text-[16px] font-bold'>Client Experiences</p>
+                            <p className='text-[#B68C5A] text-[16px] font-bold'>what we are expert at</p>
                         </div>
-                        <div className='mt-[25px] '>
-                            <h1 className='heading text-center'>our case study </h1>
+                        <div className='mt-[20px] '>
+                            <h1 className='heading'>our case study</h1>
                         </div>
                     </div>
-
+                    <p className='description mt-10 lato'>A case study showcases our firm’s expertise in resolving complex legal issues.
+                        It highlights our strategic approach, personalized client care, and commitment
+                        to delivering justice while protecting our clients rights.
+                    </p>
+                    <div className='mt-10'>
+                        <button className='flex items-center gap-2 bg-[#B68C5A] text-white rounded-lg py-4 px-6'>
+                            <p>Read More</p>
+                            <Image src='/aero.png' height={20} width={20} alt='Blog Icon' />
+                        </button>
+                    </div>
                 </div>
 
             </div>
