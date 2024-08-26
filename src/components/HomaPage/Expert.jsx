76dc01149c39576ec4163038a79edc757fc0bf0e
Jewel-190237 <190237@ku.ac.bd>
2024-08-26 11:07:13 +0600

    file uploaded

--- src/components/HomaPage/Expert.jsx
+++ src/components/HomaPage/Expert.jsx
@@ -1,10 +1,28 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Expert = () => {
     return (
-        <div>
-            Expert
-        </div>
+        <section className='max-w-[1320px] mx-auto mb-20'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                <div>
+
+                </div>
+                <div>
+                    <div className='flex flex-col items-center justify-center'>
+                        <div className='flex items-center gap-2 justify-center'>
+                            <Image src='/blogIcon.png' height={26} width={32} alt='Blog Icon' />
+                            <p className='text-[#B68C5A] text-[16px] font-bold'>Client Experiences</p>
+                        </div>
+                        <div className='mt-[25px] '>
+                            <h1 className='heading text-center'>our case study </h1>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
     );
 };
 
