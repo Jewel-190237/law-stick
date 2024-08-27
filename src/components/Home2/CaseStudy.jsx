a840e06b197546be7a12af339394fb551ce1071b
Jewel-190237 <190237@ku.ac.bd>
2024-08-27 23:30:28 +0600

    file uploaded

--- src/components/Home2/CaseStudy.jsx
+++ src/components/Home2/CaseStudy.jsx
@@ -6,7 +6,8 @@ const CaseStudy = () => {
                 <h4 className='section-heading2'>Case Study</h4>
                 <h1 className='mt-6 section-title2'>Our Recent Case Project</h1>
                 <p className='mt-10 section-description2 w-full lg:w-10/12'>
-                    Lorem ipsum dolor sit amet consectetur. Erat libero lectus dictum odio at blandit. Ultrices ornare porttitor habitant vitae et nunc. Adipiscing ultrices risus scelerisque hac etiam eget fusce faucibus morbi.
+                Discover how we have resolved complex legal challenges with expertise
+                and dedication through our successful case studies.
                 </p>
             </div>
 
