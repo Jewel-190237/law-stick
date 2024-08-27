bee8c4b2ea9d3cc7c19976d7ffe0b4c73beec8d7
Jewel-190237 <190237@ku.ac.bd>
2024-08-27 23:29:47 +0600

    file uploaded

--- src/components/Home2/CaseStudy.jsx
+++ src/components/Home2/CaseStudy.jsx
@@ -4,8 +4,7 @@ const CaseStudy = () => {
         <section className='max-w-[1320px] mx-auto'>
             <div>
                 <h4 className='section-heading2'>Case Study</h4>
-                <h1 className='mt-6 section-title2'>some good word from <br className='hidden lg:flex' />
-                    our client’s</h1>
+                <h1 className='mt-6 section-title2'>Our Recent Case Project</h1>
                 <p className='mt-10 section-description2 w-full lg:w-10/12'>
                     Lorem ipsum dolor sit amet consectetur. Erat libero lectus dictum odio at blandit. Ultrices ornare porttitor habitant vitae et nunc. Adipiscing ultrices risus scelerisque hac etiam eget fusce faucibus morbi.
                 </p>
