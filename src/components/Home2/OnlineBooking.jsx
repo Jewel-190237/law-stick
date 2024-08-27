ab4d44b08cabc12d8981f719b69858b30e311ad9
Jewel-190237 <jewel190237@gmail.com>
2024-08-27 16:05:52 +0600

    file uploaded

--- src/components/Home2/OnlineBooking.jsx
+++ src/components/Home2/OnlineBooking.jsx
@@ -10,9 +10,9 @@ const OnlineBooking = () => {
                 our expert team today, and get the personalized advice and guidance you need
             </p>
             <div className='flex flex-col md:flex-row mt-14 gap-6'>
-                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded'>
-                    <div className='py-14 pl-10'>
-                        <Image src='/personIcon.png' width={56} height={56} alt='image' />
+                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded hover:text-[#B68C5A]'>
+                    <div className='py-14 pl-10 '>
+                        <Image className='' src='/personIcon.png' width={56} height={56} alt='image' />
 
                     </div>
                     <div className='w-[1.5px] h-[78px] mx-10 mt-12 bg-[#D9CDCD]'>
@@ -24,23 +24,23 @@ const OnlineBooking = () => {
                     </div>
 
                 </div>
-                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded'>
-                    <div className='py-14 pl-10'>
-                        <Image src='/hand.png' width={56} height={56} alt='image' />
-
-                    </div>
-                    <div className='w-[1.5px] h-[78px] mx-10 mt-12 bg-[#D9CDCD]'>
-                        <br />
-                    </div>
-                    <div className='col-span-2 py-14 pr-16'>
-                        <p className='lato text-[16px] font-normal'>Book Fee </p>
-                        <h2 className='mt-4 text-[24px] font-bold lato'>Appointment</h2>
-                    </div>
-
-                </div>
-                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded'>
+                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded hover:text-[#B68C5A]'>
                     <div className='py-14 pl-10'>
                         <Image src='/pay.png' width={56} height={56} alt='image' />
+
+                    </div>
+                    <div className='w-[1.5px] h-[78px] mx-10 mt-12 bg-[#D9CDCD]'>
+                        <br />
+                    </div>
+                    <div className='col-span-2 py-14 pr-16'>
+                        <p className='lato text-[16px] font-normal'>Book Fee </p>
+                        <h2 className='mt-4 text-[24px] font-bold lato'>Appointment</h2>
+                    </div>
+
+                </div>
+                <div className='bg-[#EFF5F5] grid grid-cols-4 rounded hover:text-[#B68C5A]'>
+                    <div className='py-14 pl-10'>
+                        <Image src='/hand.png' width={56} height={56} alt='image' />
 
                     </div>
                     <div className='w-[1.5px] h-[78px] mx-10 mt-12 bg-[#D9CDCD]'>
