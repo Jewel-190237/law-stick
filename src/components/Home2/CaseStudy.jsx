13f7e8f17f0a9236ef9cdf7ad6972f18a9b83514
Jewel-190237 <190237@ku.ac.bd>
2024-08-28 00:02:44 +0600

    file uploaded

--- src/components/Home2/CaseStudy.jsx
+++ src/components/Home2/CaseStudy.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 const CaseStudy = () => {
     return (
-        <section className='max-w-[1320px] mx-auto'>
+        <section className='max-w-[1220px] mx-auto'>
             <div>
                 <h4 className='section-heading2'>Case Study</h4>
                 <h1 className='mt-6 section-title2'>Our Recent Case Project</h1>
-                <p className='mt-10 section-description2 w-full lg:w-10/12'>
-                Discover how we have resolved complex legal challenges with expertise
-                and dedication through our successful case studies.
+                <p className='mt-10 section-description2 '>
+                    Discover how we have resolved complex legal challenges with expertise <br className='hidden lg:flex' />
+                    and dedication through our successful case studies.
                 </p>
+            </div>
+            <div className='pb-12 mt-20 flex flex-col md:flex-row items-center border border-b-2 border-[#EDAAAA] border-t-0 border-l-0 border-r-0 '>
+                <div className='w-1/3'>
+                    <p className='text-[24px] font-medium lato w-full lg:w-2/3'>premier legal solution: your trusted advisor in law
+                    </p>
+                </div>
+                <div className='w-2/3'>
+                    <div className='flex items-center gap-10'>
+                        <p className='text-[16px] font-normal lato'>Explore our case studies to see how we have successfully navigated complex legal challenges <br className='hidden lg:flex' />
+                            Each case reflects our dedication, expertise, and commitment to achieving the best
+                        </p>
+                        <button className='hover:bg-[#B68C5A] border border-[#B68C5A] text-black hover:text-white rounded-lg py-4 px-8 text-[18px] lato'>View Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className='pb-12 mt-10 flex flex-col md:flex-row items-center border border-b-2 border-[#EDAAAA] border-t-0 border-l-0 border-r-0 '>
+                <div className='w-1/3'>
+                    <p className='text-[24px] font-medium lato w-full lg:w-2/3'>premier legal solution: your trusted advisor in law
+                    </p>
+                </div>
+                <div className='w-2/3'>
+                    <div className='flex items-center gap-10'>
+                        <p className='text-[16px] font-normal lato'>Explore our case studies to see how we have successfully navigated complex legal challenges <br className='hidden lg:flex' />
+                            Each case reflects our dedication, expertise, and commitment to achieving the best
+                        </p>
+                        <button className='hover:bg-[#B68C5A] border border-[#B68C5A] text-black hover:text-white rounded-lg py-4 px-8 text-[18px] lato'>View Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className='pb-12 mt-10 flex flex-col md:flex-row items-center'>
+                <div className='w-1/3'>
+                    <p className='text-[24px] font-medium lato w-full lg:w-2/3'>premier legal solution: your trusted advisor in law
+                    </p>
+                </div>
+                <div className='w-2/3'>
+                    <div className='flex items-center gap-10'>
+                        <p className='text-[16px] font-normal lato'>Explore our case studies to see how we have successfully navigated complex legal challenges <br className='hidden lg:flex' />
+                            Each case reflects our dedication, expertise, and commitment to achieving the best
+                        </p>
+                        <button className='hover:bg-[#B68C5A] border border-[#B68C5A] text-black hover:text-white rounded-lg py-4 px-8 text-[18px] lato'>View Details</button>
+                    </div>
+                </div>
             </div>
 
         </section>
