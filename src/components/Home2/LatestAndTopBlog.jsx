a5fa528257f1a4929d3ad1eb7e7687769b821913
Jewel-190237 <190237@ku.ac.bd>
2024-08-27 16:07:40 +0600

    file uploaded

--- src/components/Home2/LatestAndTopBlog.jsx
+++ src/components/Home2/LatestAndTopBlog.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const LatestAndTopBlog = props => {
     return (
-        <div>
-            
-        </div>
+        <section>
+
+            <div>
+                <h4 className='section-heading2 text-center'>online booking</h4>
+                <h1 className='mt-6 section-title2'>Schedule Your Consultation Today</h1>
+                <p className='mt-10 section-description2 w-full lg:w-1/2 mx-auto '> Take the first step toward resolving your legal issue. Book a free consultation with
+                    our expert team today, and get the personalized advice and guidance you need
+                </p>
+            </div>
+        </section>
     );
 };
 
