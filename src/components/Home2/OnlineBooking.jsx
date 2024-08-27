eea41901fa1b0b19892d51d2f10029334f8ccbeb
Jewel-190237 <jewel190237@gmail.com>
2024-08-27 15:19:37 +0600

    file uploaded

--- src/components/Home2/OnlineBooking.jsx
+++ src/components/Home2/OnlineBooking.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const OnlineBooking = () => {
     return (
-        <div>
+        <section className='max-w-[1320px] mx-auto my-20'>
+            <h4 className='section-heading2 text-center'>online booking</h4>
+            <h1 className='mt-6 section-title2'>Schedule Your Consultation Today</h1>
             
-        </div>
+        </section>
     );
 };
 
