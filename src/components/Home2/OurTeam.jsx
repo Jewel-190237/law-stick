63b410e012ad34ac781ec8ee132cd943ba84a55e
Jewel-190237 <jewel190237@gmail.com>
2024-08-28 11:05:43 +0600

    file uploaded

---
+++ src/components/Home2/OurTeam.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OurTeam = () => {
+    return (
+        <section className='max-w-[1400px] mx-auto'>
+            
+        </section>
+    );
+};
+
+
+
+export default OurTeam;
