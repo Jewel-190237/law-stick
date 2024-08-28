6b2d7ff559474521bf2f3623a4355c3ec5d08e93
Jewel-190237 <190237@ku.ac.bd>
2024-08-28 11:36:03 +0600

    file uploaded

--- src/components/Home2/OurTeam/OurTeam.jsx
+++ src/components/Home2/OurTeam/OurTeam.jsx
@@ -1,9 +1,65 @@
+'use client'
 import React from 'react';
+
+import { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import Image from 'next/image';
 
 const OurTeam = () => {
     return (
         <section className='max-w-[1400px] mx-auto'>
-            
+            <div>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+                    <SwiperSlide className='border border-red-800'>
+                        <Image src='/sliderTeam.png' width={400} height={400} alt='Slider Image' />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+
         </section>
     );
 };
